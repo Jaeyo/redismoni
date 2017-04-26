250185e6975b62fb2fd9553589b4f2cd988c9fab
Jaeyo <lastiverse@gmail.com>
2017-04-27 01:24:28 +0900

    '-'

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/client/index.js',
+  output: {
+    filename: 'public/js/bundle.js'
+  },
+  resolve: {
+    extensions: ['*', '.js', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-2']
+        }
+      },
+      {
+        test: /\.css?$/,
+        loader: 'css-loader'
+      },
+    ]
+  }
+ };
